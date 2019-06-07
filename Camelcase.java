708107ece8ade63164cdfb4ecf9297f31a9cdc45
saravana <saravanasaru14313@gmail.com>
2019-06-07 08:45:01 -0700

    Update and rename README.md to Camelcase.java

---
+++ Camelcase.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Camelcase
+{  
+  public static void main(String args[])
+  {
+	Scanner input=new Scanner(System.in);
+	String str=input.nextLine();
+	String str1=str.toLowerCase();
+	char ch[]=str1.toCharArray();
+	ch[0]=Character.toUpperCase(ch[0]);
+	for(int i=1;i<str.length();i++)
+	{
+		if(ch[i]==' ')
+		{
+			ch[i+1]=Character.toUpperCase(ch[i+1]);
+		}
+	}
+	String a=new String(ch);
+	System.out.println(a);
+  }
+}
